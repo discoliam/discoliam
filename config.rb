bab69ef46b388b9458a436109f8506736c1cd5ee
discoliam <discoliam@googlemail.com>
2013-02-20 18:00:20 +0000

    pagespeed stuff

--- config.rb
+++ config.rb
@@ -68,7 +68,7 @@ activate :asset_hash
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
   # activate :minify_javascript
